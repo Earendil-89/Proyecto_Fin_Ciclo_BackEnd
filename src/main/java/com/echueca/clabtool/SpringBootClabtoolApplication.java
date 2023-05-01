3f510fc2c2f85a836c28008dc7dfacd997a7b847
Eduardo Chueca <cm89eduardo@gmail.com>
2023-05-01 16:24:01 +0200

    Eliminados imports no utilizados

--- src/main/java/com/echueca/clabtool/SpringBootClabtoolApplication.java
+++ src/main/java/com/echueca/clabtool/SpringBootClabtoolApplication.java
@@ -2,9 +2,6 @@ package com.echueca.clabtool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -13,5 +10,4 @@ public class SpringBootClabtoolApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootClabtoolApplication.class, args);
     }
-
 }
