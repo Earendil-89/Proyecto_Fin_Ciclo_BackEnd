7bb5a069da873690a5c1a10ccbc04ccf4767bc5b
Eduardo Chueca <cm89eduardo@gmail.com>
2023-05-02 18:30:27 +0200

    corregida herencia

--- src/main/java/com/echueca/clabtool/repository/UsuarioRepository.java
+++ src/main/java/com/echueca/clabtool/repository/UsuarioRepository.java
@@ -1,11 +1,12 @@
 package com.echueca.clabtool.repository;
 
+import com.echueca.clabtool.model.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  *
  * @author Eduardo Chueca Montaner
  */
-public interface UsuarioRepository extends CrudRepository {
+public interface UsuarioRepository extends CrudRepository<Usuario, String> {
     
 }
