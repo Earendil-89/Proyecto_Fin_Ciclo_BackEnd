d7bd4e465fa92dbecee9a53aafdd1f950ba63f1c
Eduardo Chueca <cm89eduardo@gmail.com>
2023-05-02 18:29:34 +0200

    completada entidad

--- src/main/java/com/echueca/clabtool/model/Armario.java
+++ src/main/java/com/echueca/clabtool/model/Armario.java
@@ -1,15 +1,12 @@
 package com.echueca.clabtool.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-import static org.hibernate.engine.internal.Cascade.cascade;
 
 /**
  *
@@ -23,4 +20,14 @@ public class Armario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter @Setter private int id;
     @Getter @Setter private String nombre;
+    @Getter @Setter private String descripcion;
+
+    public Armario() {
+    }
+
+    public Armario(int id, String nombre, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
 }
