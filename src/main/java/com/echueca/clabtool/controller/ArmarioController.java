140fecbdfc767a819a87c9e5a66937aa2f8b3da7
Eduardo Chueca <cm89eduardo@gmail.com>
2023-05-22 21:16:16 +0200

    Soporte autenticacion

--- src/main/java/com/echueca/clabtool/controller/ArmarioController.java
+++ src/main/java/com/echueca/clabtool/controller/ArmarioController.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,26 +30,31 @@ public class ArmarioController {
     private IArmarioService armarioService;
     
     @GetMapping("/armario")
+    @PreAuthorize("hasRole('INSPECTOR') or hasRole('USER') or hasRole('ADMIN')")
     public Iterable<Armario> getArmario() throws JsonProcessingException, IOException {
         return this.armarioService.getArmario();
     }
     
     @GetMapping("/armario/{id}")
+    @PreAuthorize("hasRole('INSPECTOR') or hasRole('USER') or hasRole('ADMIN')")
     public Armario getArmarioById(@PathVariable Long id) throws JsonProcessingException, IOException { 
         return this.armarioService.getArmarioById(id);
     }
     
     @PostMapping("/armario")
+    @PreAuthorize("hasRole('INSPECTOR')")
     public ResponseEntity<?> saveArmario(@RequestBody Armario armario) throws JsonProcessingException, IOException {
         return this.armarioService.saveArmario(armario);
     }
     
     @PutMapping("/armario")
+    @PreAuthorize("hasRole('INSPECTOR')")
     public ResponseEntity<?> updateArmario(@RequestBody Armario armario) throws JsonProcessingException, IOException {
         return this.armarioService.updateArmario(armario);
     }
     
     @DeleteMapping("/armario/{id}")
+    @PreAuthorize("hasRole('INSPECTOR')")
     public ResponseEntity<?> deleteArmarioById(@PathVariable Long id) throws JsonProcessingException, IOException  {
         return this.armarioService.deleteArmario(id);
     }
