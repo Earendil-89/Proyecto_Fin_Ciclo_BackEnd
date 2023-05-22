bff1fe79e34c484e0a153b2e911d4b428d382dad
Eduardo Chueca <cm89eduardo@gmail.com>
2023-05-22 21:17:11 +0200

    Soporte autenticacion y correccion de errores

--- src/main/java/com/echueca/clabtool/controller/EstanteController.java
+++ src/main/java/com/echueca/clabtool/controller/EstanteController.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -30,31 +32,34 @@ public class EstanteController {
     private IEstanteService estanteService;
     
     @GetMapping("/estante")
-    public List<Estante> getEstante() throws JsonProcessingException, IOException {
-        return this.estanteService.getEstante();
+    @PreAuthorize("hasRole('INSPECTOR') or hasRole('USER') or hasRole('ADMIN')")
+    public List<Estante> getEstante(@RequestParam(name="armarioId", required=false) Long id) throws JsonProcessingException, IOException {
+        if( id == null ) {
+            return this.estanteService.getEstante();
+        }
+        return this.estanteService.getEstanteByArmarioId(id);
     } 
     
     @GetMapping("/estante/{id}")
+    @PreAuthorize("hasRole('INSPECTOR') or hasRole('USER') or hasRole('ADMIN')")
     public Estante getEstanteById(@PathVariable Long id) throws JsonProcessingException, IOException {
         return this.estanteService.getEstanteById(id);
     }
     
-    @GetMapping("/estante?armarioId={id}")
-    public List<Estante> getEstanteByArmarioId(@PathVariable Long id) throws JsonProcessingException, IOException {
-        return this.estanteService.getEstanteByArmarioId(id);
-    }
-    
     @PostMapping("/estante")
+    @PreAuthorize("hasRole('INSPECTOR')")
     public ResponseEntity<?> saveEstante(@RequestBody Estante estante) throws JsonProcessingException, IOException {
         return this.estanteService.saveEstante(estante);
     }
     
     @PutMapping("/estante")
+    @PreAuthorize("hasRole('INSPECTOR')")
     public ResponseEntity<?> updateEstante(@RequestBody Estante estante) throws JsonProcessingException, IOException {
         return this.estanteService.updateEstante(estante);
     }
     
     @DeleteMapping("/estante/{id}")
+    @PreAuthorize("hasRole('INSPECTOR')")
     public ResponseEntity<?> deleteEstante(@PathVariable Long id) throws JsonProcessingException, IOException {
         return this.estanteService.deleteEstante(id);
     }
